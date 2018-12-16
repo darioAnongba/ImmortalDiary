5e39780d2e9dd9ca0351127720a687534243ab0b
Dario Anongba Varela <dario.varela@consensys.net>
2018-12-16 02:22:07 +0100

    After deployment

--- migrations/2_immortal_diary.js
+++ migrations/2_immortal_diary.js
@@ -1,12 +1,13 @@
 var ImmortalDiary = artifacts.require("./ImmortalDiary.sol");
 
 module.exports = function(deployer, network) {
-  const name = "The Immortal Diary of Dario and Nati"
-  const description = "Immortal Diary of Dario Anongba Varela and Nativity Carol. Most important moments of our live. Use it wisely, as you cannot go back!"
+  const name = "The Immortal Diary of Dario and Nati";
+  const description =
+    "Immortal Diary of Dario Anongba Varela and Nativity Carol. Most important moments of our live. Use it wisely, as you cannot go back!";
 
-  if(network === 'mainnet') {
-    deployer.deploy(ImmortalDiary, name, description, {overwrite: false})
+  if (network === "mainnet") {
+    deployer.deploy(ImmortalDiary, name, description);
   } else {
-    deployer.deploy(ImmortalDiary, name, description)
+    deployer.deploy(ImmortalDiary, name, description);
   }
 };
